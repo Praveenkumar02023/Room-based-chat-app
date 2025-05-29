f6a4246df0c04a1fb276eb9868d40c12f4f3eb58
Praveen Kumar <Praveenkumar042023@gmail.com>
2025-05-29 10:38:48 +0530

    setup room logic for users, joining,sending message,recieving message.Debug for all server crash.

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,72 @@ import { WebSocketServer , WebSocket} from "ws";
 const wss = new WebSocketServer({port : 8000});
 
 let userCount =0;
-let allSockets : WebSocket[] = [];
+let allSockets = new Map<string,WebSocket[]>();
+
+
 wss.on("connection",(socket)=>{
 
     userCount += 1;
-    allSockets.push(socket);
 
     console.log("user connected #",userCount);
 
+    let roomId : string = "";
     socket.on('message',(message)=>{
 
-        allSockets.forEach(s =>{
-            s.send(message.toString());
-        });
+        const parsedMaessage = JSON.parse(message.toString());
+        console.log(parsedMaessage , "message");
+
+        if(parsedMaessage.type === "join"){
+
+            if(roomId){
+                console.log("already in room return back")
+                return;
+            }
+
+            console.log("want to join")
+            roomId = parsedMaessage.payload.roomId;
+            console.log(roomId)
+            if(!roomId) console.log("yes")
+            if(allSockets.has(roomId)){
+                allSockets.get(roomId)!.push(socket);
+            }else{
+                allSockets.set(roomId,[socket]);
+            }
+
+        }
+
+        if(parsedMaessage.type === "chat"){
+            console.log("want to chat");
+            console.log(roomId)
+            if(roomId == null) return;
+
+            allSockets.get(roomId)?.forEach((s) =>{
+                s.send(parsedMaessage.payload.message);
+            });
+
+        }
+
+    });
+
+    socket.on("close",()=>{
+
+        if(roomId){
+           const sockets  = allSockets.get(roomId);
+
+           if(sockets){
+
+                const updatedSockets = sockets?.filter((s) => s !== socket);
+
+                if(updatedSockets.length > 0){
+                    allSockets.set(roomId,updatedSockets);
+                }else{
+                    allSockets.delete(roomId);
+                }
+                console.log("removed from the map");
+
+           }
+
+        }
 
     });
     
